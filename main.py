7f6c40331452e6f2d5ad315908976d5b5c67c40e
Maggie Hakas <mhakas@uncc.edu>
2020-02-02 15:03:32 -0500

    pushing pandas trigger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -17,6 +18,11 @@ def name(value):
 def bob():
     val = {"value": "bob"}
     return jsonify(val)
+
+@app.route('/pandas')
+def pandas_sugar():
+    df = pd.read_csv("https://raw.githubusercontent.com/noahgift/sugar/master/data/education_sugar_cdc_2003.csv")
+    return jsonify(df.to_dict())
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
